9bea6014e35a9dbaeb634ceae6a4236fb52f0fc3
David Adam <zanchey@ucc.gu.uwa.edu.au>
2024-12-17 23:12:09 +0800

    add fish beta details

---
+++ Formula/fish.rb
@@ -0,0 +1,42 @@
+class Fish < Formula
+  desc "User-friendly command-line shell for UNIX-like operating systems"
+  homepage "https://fishshell.com"
+  url "https://github.com/fish-shell/fish-shell/releases/download/4.0.1/fish-4.0b1.tar.xz"
+  sha256 "534334e10f85722214e9daff82a57cc3501235523f16f8f131c2344e4ec98da7"
+  license "GPL-2.0-only"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  head do
+    url "https://github.com/fish-shell/fish-shell.git", branch: "master"
+
+    depends_on "sphinx-doc" => :build
+  end
+
+  depends_on "rust" => :build
+  depends_on "cmake" => :build
+  depends_on "pcre2"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
+                    "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}",
+                    "-Dextra_functionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d",
+                    "-Dextra_completionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d",
+                    "-Dextra_confdir=#{HOMEBREW_PREFIX}/share/fish/vendor_conf.d"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  def post_install
+    (pkgshare/"vendor_functions.d").mkpath
+    (pkgshare/"vendor_completions.d").mkpath
+    (pkgshare/"vendor_conf.d").mkpath
+  end
+
+  test do
+    system bin/"fish", "-c", "echo"
+  end
+end
